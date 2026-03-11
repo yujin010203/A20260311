69cb65c6ac293293bbd9ae6e517f343f73eb1b86
yujin010203 <yyuyu02@naver.com>
2026-03-11 17:13:56 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
 #include "StringUtil.h"
 
-
 using namespace std;
-
-
-
-
 
 int main()
 {
+	//숫자 입력 받아서 크기로 숫자 배열 만든 후
+	//1부터 크기까지 초기화하고 출력하는 프로그램
+
+	//숫자 입력 받기
+	int Size;
+	cin >> Size;
+
+	int* Number = new int[Size];
+
+	for (int i = 0; i < Size; i++) {
+		Number[i] = i + 1;
+	}
+
+	//출력
+	for (int i = 0; i < Size; i++) {
+		cout << Number[i] << " ";
+	}
+	cout << endl;
+
+	//초기화
+	delete[] Number;
+	Number = nullptr;
+
+
+	/*
 	int A = 10;
 	char Temp[100] = "Hello World";
 
@@ -19,6 +39,7 @@ int main()
 	Replace(Temp, 100, 'l', 'f');
 
 	cout << Temp << endl;
+	*/
 
 	return 0;
 }
