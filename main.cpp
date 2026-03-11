29e3485d9e9e96ec7a276dd598bf5396ebeac4ee
yujin010203 <yyuyu02@naver.com>
2026-03-11 13:23:53 +0900

    코드 수정

--- main.cpp
+++ main.cpp
@@ -7,29 +7,29 @@ int main()
 	int A[3] = { 1, 2, 3 };
 	int* P = A;
 
-	cout << A << endl;
+	//cout << A << endl;
 	//cout << *P << endl;
 
 	// 문자열 개수 세기
-	int Total = sizeof(A);
-	cout << Total << endl;
+	int Total = sizeof(A) / sizeof(int);
+	cout << "문자열의 개수 : " << Total << endl;
 
-	// 문자열 중에서 특정 문자를 특정문 바꾸기
+	// 문자열 중에서 특정 문자를 바꾸기
 	*(P+1) = 5;
-	cout << A[1] << endl;
+	cout << "문자 변경 : " << A[1] << endl;
 
 	// 문자열 중에서 특정 문자 위치 찾기
 	int Number = 3;
-	int index = -1;
+	int Index = -1;
 	for (int i = 0; i < 3; i++)
 	{
 		if (*(P+i) == Number)
 		{
-			index = i;
+			Index = i;
 			break;
 		}
 	}
-	cout << index << endl;
+	cout << Number << "의 위치 : " << Index << endl;
 
 	return 0;
 }
